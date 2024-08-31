613b141f0885863b801058d38461b5a2861b113c
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2024-08-31 19:59:34 +0700

    Added .gitignore

--- helpers/token.go
+++ helpers/token.go
@@ -1,50 +1,51 @@
 package helpers
 
 import (
-  "fmt"
-  "github.com/golang-jwt/jwt/v5"
-  "go-jwt/models"
-  "time"
+	"fmt"
+	"go-jwt/models"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 var mySigningKey = []byte("rahasiaGwehj")
 
 type MyCustomClaims struct {
-  ID    int    `json:"id"`
-  Name  string `json:"name"`
-  Email string `json:"email"`
-  jwt.RegisteredClaims
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	jwt.RegisteredClaims
 }
 
 func CreateToken(user *models.User) (string, error) {
-  claims := MyCustomClaims{
-    user.ID,
-    user.Name,
-    user.Email,
-    jwt.RegisteredClaims{
-      ExpiresAt: jwt.NewNumericDate(time.Now().Add(2 * time.Minute)),
-      IssuedAt:  jwt.NewNumericDate(time.Now()),
-      NotBefore: jwt.NewNumericDate(time.Now()),
-    },
-  }
+	claims := MyCustomClaims{
+		user.ID,
+		user.Name,
+		user.Email,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(2 * time.Minute)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
 
-  token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-  ss, err := token.SignedString(mySigningKey)
-  return ss, err
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(mySigningKey)
+	return ss, err
 }
 
 func ValidateToken(tokenString string) (any, error) {
-  token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
-    return mySigningKey, nil
-  })
-  if err != nil {
-    return nil, fmt.Errorf("unauthorized")
-  }
+	token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return mySigningKey, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unauthorized")
+	}
 
-  claims, ok := token.Claims.(*MyCustomClaims)
-  if !ok || !token.Valid {
-    return nil, fmt.Errorf("unauthorized")
-  }
+	claims, ok := token.Claims.(*MyCustomClaims)
+	if !ok || !token.Valid {
+		return nil, fmt.Errorf("unauthorized")
+	}
 
-  return claims, nil
+	return claims, nil
 }
